cbb25be0cbeba72765cb5bdd39a3c66d97a56e8c
siddharth pradhan <siddharthpradhan2004@gmail.com>
2025-01-02 16:49:26 +0530

    klines and tickers data fetching changes

--- frontend/app/components/TradeView.tsx
+++ frontend/app/components/TradeView.tsx
@@ -3,11 +3,7 @@ import { ChartManager } from "../utils/ChartManager";
 import { getKlines } from "../utils/httpClient";
 import { KLine } from "../utils/types";
 
-export function TradeView({
-  market,
-}: {
-  market: string;
-}) {
+export function TradeView({ market }: { market: string }) {
   const chartRef = useRef<HTMLDivElement>(null);
   const chartManagerRef = useRef<ChartManager>(null);
 
@@ -15,25 +11,29 @@ export function TradeView({
     const init = async () => {
       let klineData: KLine[] = [];
       try {
-        klineData = await getKlines(market, "1h", Math.floor((new Date().getTime() - 1000 * 60 * 60 * 24 * 7) / 1000), Math.floor(new Date().getTime() / 1000)); 
-      } catch (e) { }
+        klineData = await getKlines(
+          market,
+          "1h",
+          Math.floor((new Date().getTime() - 1000 * 60 * 60 * 24 * 7) / 1000),
+          Math.floor(new Date().getTime() / 1000)
+        );
+      } catch (e) {
+        console.error("Error fetching K-lines data:", e);
+      }
 
-      if (chartRef) {
+      if (chartRef.current) {
         if (chartManagerRef.current) {
           chartManagerRef.current.destroy();
         }
-        console.log(klineData)
         const chartManager = new ChartManager(
           chartRef.current,
-          [
-            ...klineData?.map((x) => ({
-              close: parseFloat(x.close),
-              high: parseFloat(x.high),
-              low: parseFloat(x.low),
-              open: parseFloat(x.open),
-              timestamp: new Date(x.end), 
-            })),
-          ].sort((x, y) => (x.timestamp < y.timestamp ? -1 : 1)) || [],
+          klineData.map((x) => ({
+            close: parseFloat(x.close),
+            high: parseFloat(x.high),
+            low: parseFloat(x.low),
+            open: parseFloat(x.open),
+            time: new Date(x.end).getTime() / 1000,
+          })),
           {
             background: "#0e0f14",
             color: "white",
@@ -44,11 +44,7 @@ export function TradeView({
       }
     };
     init();
-  }, [market, chartRef]);
+  }, [market]);
 
-  return (
-    <>
-      <div ref={chartRef} style={{ height: "520px", width: "100%", marginTop: 4 }}></div>
-    </>
-  );
+  return <div ref={chartRef} style={{ height: "520px", width: "100%", marginTop: 4 }}></div>;
 }
